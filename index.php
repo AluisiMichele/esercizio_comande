124e4ce17a4a8b51f5236d2ff7301b4c46451afa
Michele-Aluisi <michele.aluisi@isisbem.it>
2025-01-07 22:54:17 +0100

    implementato filtro funzionante

---
+++ index.php
@@ -0,0 +1,80 @@
+<DOCTYPE html>
+    <html>
+
+    <head></head>
+
+    <body>
+
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $dbname = "es_comande_Aluisi";
+
+// Create connection
+    $conn = new mysqli($servername, $username,"", $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+echo "<br>";
+
+    echo "<form method='POST'>";
+      echo "<select id='tipologie' name='filtro' id='filtComande'>";
+
+        echo"<option value='tutte'>tutte</option>";
+        echo"<option value='attivosi'>attive</option>";
+        echo"<option value='attivono'>non attive</option>";
+    
+      echo "</select>";
+
+      echo "<input type='submit' value='filtra'>";
+    echo"</form>";
+
+    echo "<table border ='1'>";
+    echo "<tr><td>ID</td><td>data</td><td>ora</td><td>stato</td><td>tavolo</td><td>coperti</td><td>id cameriere</td></tr>";
+
+    
+    $filtro = isset($_POST['filtro']) ? $_POST['filtro'] : 'tutte';
+
+    $sql = "SELECT * FROM comande";
+      if ($filtro == 'attivosi') {
+        
+        $sql .= " WHERE stato = 1";
+      
+      } elseif ($filtro == 'attivono') {
+      
+        $sql .= " WHERE stato = 0"; 
+      }
+
+    $result = $conn->query($sql);
+
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          echo "<tr>";
+          echo "<td>{$row['ID_comanda']}</td>"; 
+          echo "<td>{$row['data']}</td>";
+          echo "<td>{$row['ora']}</td>";
+          echo "<td>{$row['stato']}</td>"; 
+          echo "<td>{$row['N_tavolo']}</td>"; 
+          echo "<td>{$row['N_coperti']}</td>";
+          echo "<td>{$row['ID_cameriere']}</td>";
+          echo "</tr>";
+        
+        }
+      } else {
+        echo "0 results";
+      }
+
+      echo "</table>";
+      $conn->close();
+
+?>
+
+
+    </body>
+
+    </html>
