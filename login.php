8668cc64d4f4c3744749c6931b74b85742fa8ac5
Michele-Aluisi <michele.aluisi@isisbem.it>
2025-02-02 20:24:57 +0100

    fatto il login ma mi scrive solo in maiuscolo nell'input

---
+++ login.php
@@ -0,0 +1,68 @@
+
+<DOCTYPE html>
+    <html>
+
+    <head>
+    <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+
+    <?php
+
+session_start();
+
+include "database.php";
+
+$timeout = 900; 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+    $username = $_POST['nome_utente'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM camerieri WHERE username = ? AND pass_id = ?";
+    $query = $conn->prepare($sql);
+    $query->bind_param("ss", $username, $password);
+    $query->execute();
+    $result = $query->get_result();
+
+    if ($result->num_rows == 1) 
+    {
+        $row = $result->fetch_assoc();
+
+        $_SESSION['login'] = "OK";
+        $_SESSION['nome'] = $row['Nome'];
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['last_activity'] = time(); 
+
+        header("Location: comande.php");
+        exit();
+    } 
+    else 
+    {
+        $errore = "Credenziali errate!";
+    }
+}
+    
+
+    
+   echo "<form method='POST' action='login.php'>";
+    
+   echo "<p>Nome utente</p>";
+    echo "<input type='text' name='nome_utente' value=''>";
+    echo "<p>Password</p>";
+    echo "<input type=password name='password' value=''>";
+    echo "<br><br><input type=submit>";
+
+   echo "</form>";
+
+   if (isset($errore)) echo "<p style='color: red;'>$errore</p>";
+
+?>
+
+
+    </body>
+
+    
+    </html>
