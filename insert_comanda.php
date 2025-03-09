5c033d03fd8056aa0aed0ab697617216d8108e89
Michele-Aluisi <michele.aluisi@isisbem.it>
2025-03-09 22:43:29 +0100

    ora l'insert del dettaglio riporta a comanda.php dopo averla effettuata

--- insert_comanda.php
+++ insert_comanda.php
@@ -5,7 +5,15 @@
     $sql = "INSERT INTO dettagli_comande (ID_menu, prezzo, costo, ID_comanda)
      VALUES (" . $_POST['ID_menu'] . ", " . $_POST['prezzo'] . ", " . $_POST['costo'] . ", 1)";
     
-    $result = $conn->query($sql);   
+   // $result = $conn->query($sql);   
+
+    if ($conn->query($sql) === TRUE) {
+        // Reindirizza l'utente alla pagina precedente
+        header("Location: comanda.php");
+        exit();
+    } else {
+        echo "Errore durante l'inserimento: " . $conn->error;
+    }
 
     $conn->close();
 ?>
